32ae3f21b32a95e546f9531e42053e11fcd367a8
Jim Salyer <jim.salyer@gmail.com>
2021-05-05 11:35:54 -0500

    Automatically add createUtc and modifyUtc to API request body - JS

--- api/index.js
+++ api/index.js
@@ -20,16 +20,32 @@ const server = jsonServer.create();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
-server.post('/users/login', (req, res) => {
-  const user = router.db
-    .get('users')
-    .find({ username: req.body.username, password: req.body.password })
-    .value();
-
-  if (user) {
-    return res.json(user);
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createUtc = new Date().toISOString();
+    req.body.modifyUtc = req.body.createUtc;
+  } else if (req.method === 'PATCH' || req.method === 'PUT') {
+    req.body.modifyUtc = new Date().toISOString();
   }
-  return res.status(404);
+  next();
+});
+
+server.post('/auth/login', (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = router.db.get('users').find({ username, password }).value();
+    if (user) {
+      return res.json(user);
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+
+  return res.status(404).json({
+    message: 'The username or password is incorrect.',
+  });
 });
 
 server.use(router);
