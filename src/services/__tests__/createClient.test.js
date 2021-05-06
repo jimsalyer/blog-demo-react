0c7070cefe811190e52387f8b209e99c7e572e9a
Jim Salyer <jim.salyer@gmail.com>
2021-05-06 15:57:09 -0500

    Fix a test issue around environment variables - JS

--- src/services/__tests__/createClient.test.js
+++ src/services/__tests__/createClient.test.js
@@ -2,7 +2,18 @@ import mockedEnv from 'mocked-env';
 import createClient from '../createClient';
 
 describe('createClient()', () => {
-  const apiUrl = process.env.REACT_APP_API_URL.replace(/\/$/, '');
+  const apiUrl = 'http://www.example.com';
+  let restoreEnv;
+
+  beforeEach(() => {
+    restoreEnv = mockedEnv({
+      REACT_APP_API_URL: apiUrl,
+    });
+  });
+
+  afterEach(() => {
+    restoreEnv();
+  });
 
   it('uses the value of the environment variable REACT_APP_API_URL as the base URL of the API', () => {
     const client = createClient('test');
@@ -17,15 +28,14 @@ describe('createClient()', () => {
   });
 
   it('uses a default value when the environment variable REACT_APP_API_URL is not set', () => {
-    const restoreEnv = mockedEnv({
+    restoreEnv();
+    restoreEnv = mockedEnv({
       REACT_APP_API_URL: undefined,
     });
 
     const client = createClient('test');
 
     expect(client.defaults.baseURL).toStrictEqual(`http://localhost:5000/test`);
-
-    restoreEnv();
   });
 
   it('adds the leading forward slash to the endpoint if it is not provided', () => {
